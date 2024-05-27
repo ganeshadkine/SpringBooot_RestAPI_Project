4b0de6c75a8a81762bce7d33e131be648fe39706
Ganesh Adkine <148486222+ganeshadkine@users.noreply.github.com>
2024-05-27 09:59:14 +0530

    Add files via upload

---
+++ restapi/src/main/java/com/example/curdoperation/restapi/StudentControler.java
@@ -0,0 +1,61 @@
+package com.example.curdoperation.restapi;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentControler 
+{
+    @Autowired
+    StudentRepository repo;
+
+    //get all the Students
+    //localhost:8080/student
+    @GetMapping("/students")
+    public List<Student> getAllStudents()
+    {
+        List<Student> students =repo.findAll();
+        return students;
+    }
+    
+    //localhost:8080/students/1
+    @GetMapping("/students/{id}")
+    public Student getStudent(@PathVariable int id)
+    {
+       Student student = repo.findById(id).get();
+        return student;
+    }
+
+    @PostMapping("/student/add")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void createStudent(@RequestBody Student student)
+    {
+        repo.save(student);
+    }
+    
+    @PutMapping("/student/update/{id}")
+    public Student updateStudent(@PathVariable int id) 
+    {
+       Student student = repo.findById(id).get();
+       student.setName("kailas");
+       student.setPercentage(65);
+       repo.save(student);
+       return student;
+    }
+    @DeleteMapping("/student/delete/{id}")
+    public void removeStudent(@PathVariable int id)
+    {
+	    Student student = repo.findById(id).get();
+	    repo.delete(student);
+    }
+}
---
+++ restapi/src/main/java/com/example/curdoperation/restapi/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.example.curdoperation.restapi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> 
+{
+    
+    
+}
